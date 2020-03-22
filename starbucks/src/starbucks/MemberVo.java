80a32a4d44c7b989f10bc99cdd045ececaa77533
JuHyeon-An <57624327+JuHyeon-An@users.noreply.github.com>
2020-03-23 00:16:00 +0900

    branch test

--- starbucks/src/starbucks/MemberVo.java
+++ starbucks/src/starbucks/MemberVo.java
@@ -7,6 +7,15 @@ public class MemberVo {
 	String member_pw;
 	String member_name;
 	
+	//branch 작업
+	
+	String member_phone;
+	String member_address;
+	String member_email;
+	String member_regDate;
+	String member_savedmoney;
+	String member_admin;
+	
 	public String getMember_id() {
 		return member_id;
 	}
@@ -25,5 +34,43 @@ public class MemberVo {
 	public void setMember_name(String member_name) {
 		this.member_name = member_name;
 	}
+	public String getMember_phone() {
+		return member_phone;
+	}
+	public void setMember_phone(String member_phone) {
+		this.member_phone = member_phone;
+	}
+	public String getMember_address() {
+		return member_address;
+	}
+	public void setMember_address(String member_address) {
+		this.member_address = member_address;
+	}
+	public String getMember_email() {
+		return member_email;
+	}
+	public void setMember_email(String member_email) {
+		this.member_email = member_email;
+	}
+	public String getMember_regDate() {
+		return member_regDate;
+	}
+	public void setMember_regDate(String member_regDate) {
+		this.member_regDate = member_regDate;
+	}
+	public String getMember_savedmoney() {
+		return member_savedmoney;
+	}
+	public void setMember_savedmoney(String member_savedmoney) {
+		this.member_savedmoney = member_savedmoney;
+	}
+	public String getMember_admin() {
+		return member_admin;
+	}
+	public void setMember_admin(String member_admin) {
+		this.member_admin = member_admin;
+	}
+
+	
 	
 }
