7884155c7782c1927d3a05e8a0acd97b7eefc1a4
JuHyeon-An <57624327+JuHyeon-An@users.noreply.github.com>
2020-03-23 00:12:57 +0900

    memberVo

---
+++ starbucks/src/starbucks/MemberVo.java
@@ -0,0 +1,29 @@
+//MemberVo for branch test
+package starbucks;
+
+public class MemberVo {
+
+	String member_id;
+	String member_pw;
+	String member_name;
+	
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getMember_pw() {
+		return member_pw;
+	}
+	public void setMember_pw(String member_pw) {
+		this.member_pw = member_pw;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+	
+}
