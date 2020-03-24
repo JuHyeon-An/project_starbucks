14dd65a0cdac9331a288f3b453665e229a8ede12
JuHyeon-An <57624327+JuHyeon-An@users.noreply.github.com>
2020-03-24 17:11:06 +0900

    .

--- starbucks/src/bean/DBConn.java
+++ starbucks/src/bean/DBConn.java
@@ -6,14 +6,12 @@ import java.sql.DriverManager;
 public class DBConn {
 	public static Connection getConn() {
 		String driver = "oracle.jdbc.driver.OracleDriver";
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		String dbUser = "starbucks";
-		String dbPwd = "1234";
-		/*String url = "jdbc:oracle:thin:@192.168.0.32:1521:xe";
+//		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+//		String dbUser = "starbucks";
+//		String dbPwd = "1234";
 		String url = "jdbc:oracle:thin:@192.168.0.32:1521:xe";
-//		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String dbUser = "hr";
-		String dbPwd = "hr";*/
+		String dbPwd = "hr";
 		Connection conn = null;
 		
 		try {
