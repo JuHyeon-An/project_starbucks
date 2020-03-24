ee84ea6a8e6638fe1a10a6b549404c8ee397c05f
sw <hi@localhost>
2020-03-24 09:58:42 +0900

    sw

--- starbucks/WebContent/admin/order.js
+++ starbucks/WebContent/admin/order.js
@@ -3,15 +3,25 @@
  */
 
 
+//let func=function(){
+//	if($('#btnSelect')!=null){
+//		$('#btnSelect').on('click', function(){
+//			$('#frm').attr('action','order.order').submit();
+//		});
+//	}
+//}
+
+
 let func=function(){
-	if($('#btnSelect')=!null){
-		$('#btnSelect').on('click', function(){
-			$('#frm').attr('action','order.star').submit();
-		});
+	if($('#btnSelect')!=null){
+		$('#btnSelect').click(function(){
+			$('#frm').attr('action' , 'order.order').submit();
+		})
 	}
 }
 
+
 let init = function(){
-	$('#frm').attr('action','order.star').submit();
+	$('#frm').attr('action','order.order').submit();
 	func();
 }
