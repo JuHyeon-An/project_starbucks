04c22f5b57862d72d9ac6bd0b7b33f3cc806373b
GwanUkKim <guk1502012@gamil.com>
2020-03-27 13:51:19 +0900

    review_insert_select_doing

--- starbucks/src/bean/DBConn.java
+++ starbucks/src/bean/DBConn.java
@@ -8,6 +8,10 @@ public class DBConn {
 	public static Connection getConn() {
 		String driver = "oracle.jdbc.driver.OracleDriver";
 //		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+<<<<<<< HEAD
+=======
+//		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+>>>>>>> 2f3b3634a4e5741a144421637a79081fb49f4a8d
 //		String dbUser = "starbucks";
 //		String dbPwd = "1234";
 		String url = "jdbc:oracle:thin:@192.168.0.32:1521:xe";
